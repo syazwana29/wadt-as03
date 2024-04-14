e1ceb51994a0c5b79d306f138a874f284c2c7168
Syazwana <22FTT1515@student.pb.edu.bn>
2024-04-14 10:46:18 +0800

    modified profile picture size and position & added name

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,29 @@ defineExpose({ profile });
 
 <template>
   <div>
-    <img :src="profile" />
+    <img class="profile-image" :src="profile" />
+    <p class="name">DAYANGKU SYAZWANA</p>
   </div>
+
+  <div></div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.profile-image {
+  border-radius: 50%;
+  position: absolute;
+  left: 50px;
+  top: 65px;
+  width: 350px;
+  height: 350px;
+}
+
+.name {
+  font-size: 25px;
+  font-weight: bold;
+  position: absolute;
+  left: 90px;
+  top: 450px;
+  color: #000;
+}
+</style>
