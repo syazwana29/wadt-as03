562b0869bbe25012c58055fdcc8d3b4027e9f2d8
Syazwana <22FTT1515@student.pb.edu.bn>
2024-04-14 10:27:45 +0800

    added profile picture

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { defineExpose } from "vue";
+import profile from "./assets/profile.jpeg";
+
+defineExpose({ profile });
+</script>
 
 <template>
-  <div></div>
+  <div>
+    <img :src="profile" />
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
