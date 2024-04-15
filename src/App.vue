52a00aaef2d9614a53bde4d15ca8f0755e8745cd
Syazwana <22FTT1515@student.pb.edu.bn>
2024-04-15 10:45:01 +0800

    converted profile style to tailwindcss version

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,15 @@ defineExpose({ profile });
 
 <template>
   <div>
-    <img class="profile-image" :src="profile" />
-    <p class="name">DAYANGKU SYAZWANA</p>
+    <img :src="profile" class="w-80 h-80 rounded-full absolute ml-12 mt-16" />
+    <p>DAYANGKU SYAZWANA</p>
   </div>
 
   <div>
-    <h2 class="header">
+    <h2>
       Dayangku Syazwana Pengiran Haji Ismail Kairuluddin, 22FTT1515
     </h2>
-    <p class="description">
+    <p>
       I am currently studying in Politeknik Brunei, taking level 5 Diploma in
       Web Development. Devoted to volleyball and loves creating and designing as
       a way to show my creativity. Likes playing video games to explore virtual
@@ -23,8 +23,8 @@ defineExpose({ profile });
     </p>
   </div>
 
-  <div class="contact-details">
-    <p style="font-weight: bold">CONTACT</p>
+  <div>
+    <p>CONTACT</p>
     <p>22FTT1515@student.pb.edu.bn</p>
     <p>+673 xxxxxxx</p>
     <p>www.syazwana.io</p>
@@ -32,40 +32,32 @@ defineExpose({ profile });
 </template>
 
 <style scoped>
-.profile-image {
-  border-radius: 50%;
-  position: absolute;
-  left: 50px;
-  top: 65px;
-  width: 350px;
-  height: 350px;
-}
 
-.name {
+/* .name {
   font-size: 25px;
   font-weight: bold;
   position: absolute;
   left: 95px;
   top: 450px;
   color: #000;
-}
+} */
 
-.header {
+/* .header {
   font-size: 25px;
   font-weight: bold;
   color: #000;
   margin-bottom: 60px;
   margin-left: 400px;
-}
+} */
 
-.description {
+/* .description { text-xl ml-400 mb-90
   font-size: 20px;
   color: #000;
   margin-left: 400px;
   margin-bottom: 90px;
-}
+} */
 
-.contact-details {
+/* .contact-details {
   margin-left: 350px;
-}
+} */
 </style>
