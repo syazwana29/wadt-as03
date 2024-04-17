08bd7c3df8d4d21985de2eae1be53f002b7fb971
Syazwana <22FTT1515@student.pb.edu.bn>
2024-04-18 07:35:03 +0800

    changed base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/syazwana29/wadt-as03',
+  base: '/wadt-as03',
   plugins: [vue()],
 }) 
